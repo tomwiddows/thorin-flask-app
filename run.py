58784b8e0dbbac41db26ad168a0356f06f27716f
Tom Widdows <thomas.widdows10@gmail.com>
2024-06-21 12:23:36 +0000

    Import flash. Add with statement and if statement to contact.html to display flashed messageupon form sumbission

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
@@ -28,7 +31,7 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form.get("name"))
+        flash("Thanks {}, we have received your message!".format(request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 @app.route("/careers")
